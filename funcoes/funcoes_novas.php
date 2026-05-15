4cb7bc7e8a2aa9e0433cef6320d1729f6bf0d0ea
Taynan19 <taynan10gk@gmail.com>
2026-05-15 14:41:05 -0300

    lolllllllllllllllll

--- funcoes/funcoes_novas.php
+++ funcoes/funcoes_novas.php
@@ -1,4 +1,29 @@
+<?php
+
+    session_start();
+
+
+
+
+?>
+
+
+
 <script>
+
+    // Função para pesquisar 
+
+    // Função para cadastrar usuariono banco de dados ( tem que colocar as imagens )
+
+    function cadastrarUsuario($conexao,$nome, $idade, $cpf, $sexo, $email, $senha, $img, $estado_id, $cidade_id) {
+        $sql = "INSERT INTO usuarios ( usuarios_nome, usuarios_idade, usuarios_cpf, usuarios_sexo, usuarios_email, usuarios_senha, usuarios_img ) VALUES (?, ?, ?, ?, ?, ?, ? )";
+        $comando = mysql_prepare($conexao, $sql);
+        mysqli_stmt_bind_param($comando, "sssssss", $nome, $idade, $cpf, $sexo, $email, $senha, $img );
+
+
+    }
+    // Função para listar os estados e cidades do banco de dados
+
     function listar($conexao) {
     $sql = "SELECT * FROM estado ORDER BY estado_nome";
     $comando = mysqli_prepare($conexao, $sql);
@@ -11,4 +36,20 @@
     mysqli_stmt_close($comando);
     return $lista_estados;
     }
+
+    function listarCidades($conexao, $estado_id) {
+    $sql = "SELECT * FROM cidade WHERE estado_id = ? ORDER BY cidade_nome";
+    $comando = mysqli_prepare($conexao, $sql);
+    mysqli_stmt_bind_param($comando, 'i', $estado_id);
+    mysqli_stmt_execute($comando);
+    $resultado = mysqli_stmt_get_result($comando);
+    $lista_cidades = [];
+    while ($cidade = mysqli_fetch_assoc($resultado)) {
+        $lista_cidades[] = $cidade;
+    }
+    mysqli_stmt_close($comando);
+    return $lista_cidades;
+    }
+
+
 </script>
